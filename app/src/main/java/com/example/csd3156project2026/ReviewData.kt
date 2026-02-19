fe33d001851853c9db907498938168f7711d425c
Arshad <arshad1999@live.com>
2026-02-20 01:34:45 +0800

    UI Flow for Rating Page

---
+++ app/src/main/java/com/example/csd3156project2026/ReviewData.kt
@@ -0,0 +1,11 @@
+package com.example.csd3156project2026
+
+data class ReviewData(
+    val markerId: String,   // Links the review to a specific map marker
+    val userId: String,
+    val rating: Int,
+    val comment: String,
+    val imageUrl: String? = null    // Store the link to the image here
+) {
+
+}
